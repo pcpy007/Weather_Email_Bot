e2b2943f75213a86c8248f51f35babada890ece1
nannan7 <pcpy007@gmail.com>
2023-02-26 20:39:31 +0800

    Update weather.sh

--- weather.sh
+++ weather.sh
@@ -4,5 +4,5 @@
 ppp=`curl wttr.in/Shanghai?format=\"%l:+%c+%t+%f+%w+%S+%s\"\&m`
 ppp1=`echo ${ppp//\"/}`
 ARR=($ppp1)
-OUT="天气:${ARR[1]} \n最高气温:${ARR[3]} \n最低气温:${ARR[4]} \n风向风速:${ARR[5]} 日出时间:${ARR[6]} 日落时间:${ARR[7]}"
+OUT=`echo -e "天气:${ARR[1]} \n最高气温:${ARR[2]} \n最低气温:${ARR[3]} \n风向风速:${ARR[4]} 日出时间:${ARR[5]} 日落时间:${ARR[6]}"`
 echo $OUT
