913378cf26b8816b973a75306c1729e59d8863f1
nannan7 <pcpy007@gmail.com>
2023-02-26 20:17:17 +0800

    try

--- weather.sh
+++ weather.sh
@@ -7,8 +7,11 @@ LANGUAGE="zh-cn"
 UNIT=m
 UA="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.88 Safari/537.36"
 
-curl \
+ppp=curl \
     -H "Accept-Language: $LANGUAGE" \
     -H "User-Agent: $UA" \
-    -o result.html \
-    wttr.in/$CITY?format=4\&$UNIT
+    wttr.in/Shanghai?format=\"%l:+%c+%C+%t+%f+%w+%S+%s\"\&m
+ppp1=`${ppp//\"/}`
+ARR=($ppp1)
+OUT="天气:${ARR[1]} \n最高气温:${ARR[3]} \n最低气温:${ARR[4]} \n风向风速:${ARR[5]} 日出时间:${ARR[6]} 日落时间:${ARR[7]}"
+echo $OUT
